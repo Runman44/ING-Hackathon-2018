694e21622efe0f59670e2e98db359a15f1c5dd92
Rohan Dudhane <rohan.dudhane@ing.nl>
2018-03-21 21:09:56 +0100

    parcelize all the things!

--- app/src/main/java/nl/mranderson/hackathon2018/splitpayment/SplitPaymentData.kt
+++ app/src/main/java/nl/mranderson/hackathon2018/splitpayment/SplitPaymentData.kt
@@ -1,31 +1,8 @@
 package nl.mranderson.hackathon2018.splitpayment
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class SplitPaymentData(val corporateAmount: String,
-                            val personalAmount: String) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString()) {
-    }
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeString(corporateAmount)
-        dest?.writeString(personalAmount)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<SplitPaymentData> {
-        override fun createFromParcel(parcel: Parcel): SplitPaymentData {
-            return SplitPaymentData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<SplitPaymentData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+                            val personalAmount: String) : Parcelable
