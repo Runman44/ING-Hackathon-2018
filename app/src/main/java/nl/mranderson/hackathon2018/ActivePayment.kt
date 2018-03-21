ddfa65fab9764d0f5025e1231f1c34b535a29fb9
Olaf Achthoven <olaf.achthoven@maxdoro.nl>
2018-03-21 21:26:50 +0100

    Add transaction amount

--- app/src/main/java/nl/mranderson/hackathon2018/ActivePayment.kt
+++ app/src/main/java/nl/mranderson/hackathon2018/ActivePayment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_active_payment.*
+import nl.mranderson.hackathon2018.R.id.paymentTotal
 import nl.mranderson.hackathon2018.card.CardImageFragment
 import nl.mranderson.hackathon2018.data.Transaction
 
@@ -21,6 +23,15 @@ class ActivePayment : AppCompatActivity() {
                     .add(R.id.cardFragment, fragment, "CARDIMAGETAG")
                     .commit()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val transaction = intent.getParcelableExtra<Transaction>(KEY_TRANSACTION);
+
+        paymentTotal.text = (transaction.amount.valueInCents / 100.0f).toString()
+
     }
 
     companion object {
