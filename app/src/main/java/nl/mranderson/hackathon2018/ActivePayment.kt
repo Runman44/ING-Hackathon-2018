d6ef10d1a9e2d357150c597db0104b467591ffd3
Olaf Achthoven <olaf.achthoven@maxdoro.nl>
2018-03-21 17:59:44 +0100

    NFC read

--- app/src/main/java/nl/mranderson/hackathon2018/ActivePayment.kt
+++ app/src/main/java/nl/mranderson/hackathon2018/ActivePayment.kt
@@ -1,12 +1,20 @@
 package nl.mranderson.hackathon2018
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ActivePayment : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_selector)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        Toast.makeText(this, "Found TAG", Toast.LENGTH_LONG).show()
     }
 }
