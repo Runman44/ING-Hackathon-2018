a6f446bf147422aaf696b367a851b32d6fb06c38
Dennis Anderson <askmranderson@gmail.com>
2018-03-21 18:14:00 +0100

    beginning of the cards image fragment

--- app/src/main/java/nl/mranderson/hackathon2018/card/CardFragment.kt
+++ app/src/main/java/nl/mranderson/hackathon2018/card/CardFragment.kt
@@ -4,9 +4,12 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_list.*
 import nl.mranderson.hackathon2018.R
 
 class CardFragment : Fragment() {
@@ -24,12 +27,30 @@ class CardFragment : Fragment() {
         val presenter = CardPresenter(viewState, CardInteractor())
         viewModel = ViewModelProviders.of(this, VideoViewModelFactory(presenter, viewState)).get(CardViewModel::class.java)
 
+        val adapter = CardSlidePagerAdapter(fragmentManager)
+        adapter.addFragment(CardImageFragment())
+        pager.adapter = adapter
         bindViews()
     }
 
     private fun bindViews() {
-        //TODO
+        //TODO when data is
         viewModel.viewState.data.observe(this, Observer { })
     }
-}
 
+    class CardSlidePagerAdapter(supportFragmentManager: FragmentManager?) : FragmentStatePagerAdapter(supportFragmentManager) {
+        private var list: ArrayList<Fragment> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+            return list[position]
+        }
+
+        override fun getCount(): Int {
+            return list.size
+        }
+
+        fun addFragment(fragment: Fragment) {
+            list.add(fragment)
+        }
+    }
+}
---
+++ app/src/main/java/nl/mranderson/hackathon2018/card/CardImageFragment.kt
@@ -0,0 +1,8 @@
+package nl.mranderson.hackathon2018.card
+
+import android.support.v4.app.Fragment
+
+class CardImageFragment : Fragment() {
+
+}
+
