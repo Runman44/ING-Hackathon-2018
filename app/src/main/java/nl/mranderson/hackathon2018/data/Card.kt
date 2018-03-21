8ca74561894dea1d41d2a7e2294e0407a95e71e6
Dennis Anderson <askmranderson@gmail.com>
2018-03-21 21:15:14 +0100

    implement multidex and firestone

--- app/src/main/java/nl/mranderson/hackathon2018/data/Card.kt
+++ app/src/main/java/nl/mranderson/hackathon2018/data/Card.kt
@@ -1,9 +1,11 @@
 package nl.mranderson.hackathon2018.data
 
+import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.parcel.RawValue
 
+@SuppressLint("ParcelCreator")
 @Parcelize
 data class Card(val iban: String,
                 val accountId: String,
